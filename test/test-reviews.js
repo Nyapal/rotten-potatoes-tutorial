220bfe933c9f46a3111cffec2bbb8518eae8058e
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-09-20 16:53:25 -0700

    testing working

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -1,8 +1,14 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app');
+const server = require('../app');
 const should = chai.should();
 const Review = require('../models/review');
+//const sampleMovieId = '2143056897'
+const sampleReview = {
+    "title": 'Sample Data',
+    "movie-title": 'Test Movie',
+    "description": 'This here is sample data'
+}
 
 chai.use(chaiHttp);
 
@@ -15,13 +21,68 @@ describe('Reviews', () => {
             res.should.have.status(200);
             res.should.be.html;
             done();
-        })
-    })
+        });
+    });
     // TEST NEW
-    // TEST CREATE
-    // TEST SHOW
-    // TEST EDIT
-    // TEST UPDATE
-    // TEST DELETE
+    it('should should show new review form on /GET', (done) => {
+        chai.request(server)
+        .get('/reviews/new')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.html;
+            done();
+        });
+    });
+    // // TEST CREATE
+    it('should create a new review on /POST', (done) => {
+        chai.request(server)
+        .post('/reviews')
+        .send(sampleReview)
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.html;
+            done();
+        });
+    });
+    // // TEST SHOW
+    // it('should index ALL reviews on /SHOW', (done) => {
+    //     chai.request(server)
+    //     .get('/')
+    //     .end((err, res) => {
+    //         res.should.have.status(200);
+    //         res.should.be.html;
+    //         done();
+    //     });
+    // });
+    // // TEST EDIT
+    // it('should index ALL reviews on /GET', (done) => {
+    //     chai.request(server)
+    //     .get('/')
+    //     .end((err, res) => {
+    //         res.should.have.status(200);
+    //         res.should.be.html;
+    //         done();
+    //     });
+    // });
+    // // TEST UPDATE
+    // it('should index ALL reviews on /GET', (done) => {
+    //     chai.request(server)
+    //     .get('/')
+    //     .end((err, res) => {
+    //         res.should.have.status(200);
+    //         res.should.be.html;
+    //         done();
+    //     });
+    // });
+    // // TEST DELETE
+    // it('should index ALL reviews on /GET', (done) => {
+    //     chai.request(server)
+    //     .get('/')
+    //     .end((err, res) => {
+    //         res.should.have.status(200);
+    //         res.should.be.html;
+    //         done();
+    //     });
+    // });
 
-})
+});
