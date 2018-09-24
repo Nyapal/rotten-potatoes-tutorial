c7e68d1abba895604c0310e6133c407d4e7d6905
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-09-24 14:33:07 -0700

    testing working

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -3,11 +3,12 @@ const chaiHttp = require('chai-http');
 const server = require('../app');
 const should = chai.should();
 const Review = require('../models/review');
-//const sampleMovieId = '2143056897'
+const sampleMovieId = '2143056897'
 const sampleReview = {
     "title": 'Sample Data',
     "movie-title": 'Test Movie',
-    "description": 'This here is sample data'
+    "description": 'This here is sample data',
+    "id": sampleMovieId
 }
 
 chai.use(chaiHttp);
@@ -44,45 +45,64 @@ describe('Reviews', () => {
             done();
         });
     });
-    // // TEST SHOW
-    // it('should index ALL reviews on /SHOW', (done) => {
-    //     chai.request(server)
-    //     .get('/')
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html;
-    //         done();
-    //     });
-    // });
-    // // TEST EDIT
-    // it('should index ALL reviews on /GET', (done) => {
-    //     chai.request(server)
-    //     .get('/')
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html;
-    //         done();
-    //     });
-    // });
-    // // TEST UPDATE
-    // it('should index ALL reviews on /GET', (done) => {
-    //     chai.request(server)
-    //     .get('/')
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html;
-    //         done();
-    //     });
-    // });
-    // // TEST DELETE
-    // it('should index ALL reviews on /GET', (done) => {
-    //     chai.request(server)
-    //     .get('/')
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html;
-    //         done();
-    //     });
-    // });
+    // TEST SHOW
+    it('should show a single review on /reviews/id/ GET', (done) => {
+        let test = new Review(sampleReview);
+        test.save((err, data) => {
+            chai.request(server)
+                .get(`/reviews/${data._id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    done();
+                });
+        });
+    });
+    // TEST EDIT
+    it('should index ALL reviews on /GET', (done) => {
+        let test = new Review(sampleReview);
+        test.save((err, data) => {
+            chai.request(server)
+                .get(`/reviews/${data._id}/edit`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
+    });
+    // TEST UPDATE
+    it('should update single review on /reviews/id PUT', (done) => {
+        let test = new Review(sampleReview);
+        test.save((err, data) => {
+            chai.request(server)
+                .put(`/reviews/${data._id}?_method=PUT`)
+                .send({'title': 'Updating the title'})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
+    });
+    // TEST DELETE
+    it('should delete a single review on /reviews/id DELETE', (done) => {
+        let test = new Review(sampleReview);
+        test.save((err, data) => {
+            chai.request(server)
+                .delete(`/reviews/${data._id}?_method=DELETE`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
+    });
+
+    after(() => {
+        Review.deleteMany({title: 'Sample Data'}).exec((err, reviews) => {
+            reviews.remove();
+        })
+    });
 
 });
