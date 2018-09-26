eecb189834a56f00bf752a085460dda971333d56
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-09-26 10:37:46 -0700

    testing deploy

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const reviews = require('./controllers/reviews.js');
 const app = express();
+
 var exphbs = require('express-handlebars');
+
+var mongoose = require('mongoose');
+
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -12,8 +17,19 @@ app.use(methodOverride('_method'))
 
 reviews(app)
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//     console.log('App listening on port 3000!')
+// })
+
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('App listening on port 3000!')
+    const db = process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes';
+    mongoose.connect(db);
 })
 
 module.exports = app;
