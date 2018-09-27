af69f6bed6acb8a63c4361d0c91f804750fb287c
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-09-27 16:05:24 -0700

    adding movie posters

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,27 +1,26 @@
-const Review = require('../models/review.js');
-const Comment = require('../models/comment.js');
 const MovieDb = require('moviedb-promise');
 const moviedb = new MovieDb('68eb466ca897baf7a62975f97c5f32bc')
-function movies(app) {
+const Review = require('../models/review.js');
+const Comment = require('../models/comment.js');
+
+function movies (app) {
     app.get('/', (req, res) => {
-        moviedb.miscNowPlayingMovies().then(response => {
+        moviedb.searchMovie({ query: 'The Lion King' }).then(response => {
+            console.log({movies: response.results})
             res.render('movies-index', {movies: response.results})
-        }).catch(console.console.error)
+        }).catch(console.error)
     })
 }
 
 
-// function movies (app) {
-//     app.get('/', (req, res) => {
-//         req.render('movies-index')
-//         // Review.find()
-//         //     .then(reviews => {
-//         //         res.render('reviews-index', {reviews: reviews});
-//         //     })
-//         //     .catch(err => {
-//         //         console.log(err);
-//         //     });
-//     //});
+
+// app.get('/', (req, res) => {
+//     moviedb.searchMovie({query: 'Alien'}).then(response => {
+//     //moviedb.miscNowPlayingMovies().then(response => {
+//         res.render('movies-index', {movies: response.results})
+//     }).catch(err => {
+//         console.log(err)
+//     })
 // })
 
 module.exports = movies
